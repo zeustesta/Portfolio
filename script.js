afd8c04b9ddc84f64aa2c5e05a97d4d751063eb4
zeuse <zeuserosmardel@hotmail.com.ar>
2024-03-28 19:22:13 -0300

    Finished styles and structure. Starting to change text fields.

--- script.js
+++ script.js
@@ -2,3 +2,25 @@ function openMenu() {
   const nav = document.querySelector('nav');
   nav.classList.toggle('open');
 }
+
+function downloadPDF() {
+  var link = document.createElement('a');
+  link.style.display = 'none';
+  
+  link.href = '/assets/CV/CV.pdf';
+  
+  link.download = 'Curriculum - Testa Zeus';
+  
+  document.body.appendChild(link);
+  
+  link.click();
+  
+  document.body.removeChild(link);
+}
+
+function copyEmail() {
+  let email = "zeusjtesta@gmail.com";
+
+  navigator.clipboard.writeText(email);
+  alert("Copied email");
+}

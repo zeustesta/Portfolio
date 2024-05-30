5818b1355e4fe1d237a82e1fcf6d27cd67b6a905
zeuse <zeuserosmardel@hotmail.com.ar>
2024-05-29 22:00:56 -0300

    cv update

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function downloadPDF() {
   var link = document.createElement('a');
   link.style.display = 'none';
   
-  link.href = 'assets/CV/Cv - Zeus Testa.docx';
+  link.href = 'assets/CV/Cv - Testa Zeus.docx';
   
   link.download = 'Curriculum - Testa Zeus';
   
